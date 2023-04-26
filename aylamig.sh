077cac36927189afab891cfbd0201574fc470245
chilumbugeorge <chilumbu.george@gmail.com>
2023-04-26 14:57:54 +0800

    Update script

--- aylamig.sh
+++ aylamig.sh
@@ -13,12 +13,12 @@ do
         u) username=${OPTARG};;
         h) hostname=${OPTARG};;
         d) database=${OPTARG};;
-        s) service=${OPTARG};;
+        s) cluster=${OPTARG};;
         c) create=${OPTARG};;
         C) cmds=${OPTARG};;
         v) version=${OPTARG};;
-        r) su_num=${OPTARG};;
-        t) app_version=${OPTARG};;
+        r) infdba=${OPTARG};;
+        t) chg=${OPTARG};;
         m) migrate=${OPTARG};;
         i) info=${OPTARG};;
         l) list=${OPTARG};;
@@ -36,29 +36,29 @@ Options:
   -u, --username <n>              database username -u <uername>
   -h, --hostname <levels>         database hostname -h <host>
   -d, --database                  database schema name -d <db-name>
-  -s, --service                   application service e.g. ads, rules, feed, user etc -s <service-name>
+  -s, --cluster                   application service e.g. main, trades, ref etc -s <db-cluster>
   -c, --script                    file script with extension .json or .sql e.g. alter_table_employee_add_column_name.sql
-  -v, --db-version                to track database versions. Automatically created.
-  -r, --SU                        to track Jira SU tickets -r <SU-num>
-  -t, --app-version               to track application migration version
+  -v, --version                   to track database versions. Automatically created.
+  -r, --infdba                    to track Jira infdba tickets -r <infdba>
+  -t, --chg                       to track application migration version
   -C, --Command                   action control command -C [base | create | list | info | pull | push | migrate | rollback]
       base                        -C base -u <db-user> -h <db-host> -d <db-name>
-      create                      -C create -s <service-name> -c <file-script> -v <db-version>
-      info                        -C info -s <service-name> -u <db-user> -h <db-host> -d <db-name> -v <db-migration-version>
-      list                        -C list -s <service-name>
-      migrate                     -C migrate -s <service-name> -u <db-user> -h <db-host> -d <db-name> -v <db-migration-version> -r <app-version -t <SU>
-      pull                        -C pull -s <service-name> -u <db-user> -h <db-host> -d <db-name> 
-      push                        -C pull -s <service-name> -u <db-user> -h <db-host> -d <db-name> 
-      version                     -C version -s <service-name>
+      create                      -C create -s <db-cluster> -c <file-script> -v <version>
+      info                        -C info -s <db-cluster> -u <db-user> -h <db-host> -d <db-name> -v <version>
+      list                        -C list -s <db-cluster>
+      migrate                     -C migrate -s <db-cluster> -u <db-user> -h <db-host> -d <db-name> -v <version> -r <chg> -t <infdba>
+      pull                        -C pull -s <db-cluster> -u <db-user> -h <db-host> -d <db-name> 
+      push                        -C pull -s <db-cluster> -u <db-user> -h <db-host> -d <db-name> 
+      version                     -C version -s <db-cluster>
 "
 
 
 now=$(date +%Y%m%d%H%M%S)
 alter_template="templates/alter.json"
-servicedir="migrations/$service"
-versiondir="$servicedir/$version"
+clusterdir="migrations/$cluster"
+versiondir="$clusterdir/$version"
 
-if [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$username" ] && [ ! -z "$hostname" ] && [ ! -z "$database" ] && [ ! -z "$create" ] && [ ! -z "$version" ] && [ ! -z "$app_version" ] && [ ! -z "$su_num" ]; then
+if [ ! -z "$cmds" ] && [ ! -z "$cluster" ] && [ ! -z "$username" ] && [ ! -z "$hostname" ] && [ ! -z "$database" ] && [ ! -z "$create" ] && [ ! -z "$version" ] && [ ! -z "$chg" ] && [ ! -z "$infdba" ]; then
     if [ "$cmds" == "migrate" ]; then
         read -r -p "Are You Sure About Running Migration ${MAG}$create${NC}[Y/n] " input 
         case $input in
@@ -83,9 +83,8 @@ if [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$username" ] && [ ! -z "$h
 	if [ "$ext" == "json" ]; then
             T=$(jq -r '.table' $versiondir/$create)
             S=$(jq -r '.change' $versiondir/$create)
-            #pt-online-schema-change --charset utf8 --chunk-size 1000 --max-load Threads_connected=800 --user ${username} --pass ${password} --alter "${S}" --host ${hostname} --max-lag=300 --recursion-method=none  D=${database},t=${T} --execute &&
-            pt-online-schema-change --charset utf8 --check-unique-key-change --chunk-size 1000 --max-load Threads_connected=8000 --no-drop-old-table --user ${username} --pass ${password} --alter "${S}" --host ${hostname} --max-lag=500 --recursion-method=none  D=${database},t=${T} --execute &&
-            mysql -u${username} -h${hostname} -p${password} ${database} -e "INSERT INTO SCHEMA_CHANGE_LOGS VALUES(null,'$version','$app_version','$su_num','$username','$T','ALTER TABLE','$create',NOW())";
+	    pt-online-schema-change --chunk-size 1000 -h ${hostname} --user ${username} --max-load Threads_running=1500 --critical-load Threads_running=1500 --no-drop-old-table --ask-pass --recursion-method=none --max-lag 4s --alter "${S}" D=${database},t=${T} --execute &&
+            mysql -u${username} -h${hostname} -p${password} ${database} -e "INSERT INTO SCHEMA_CHANGE_LOGS VALUES(null,'$version','$chg','$infdba','$username','$T','ALTER TABLE','$create',NOW())";
 	    exit 0
 	elif [ "$ext" == "sql" ]; then
 	    C=$(mysql -u${username} -h${hostname} -p${password} ${database} < $versiondir/$create);
@@ -100,14 +99,14 @@ if [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$username" ] && [ ! -z "$h
                     if [ ! -z "$ddl_create" ];then
                         echo $version
 
-                        mysql -u${username} -h${hostname} -p${password} ${database} -e "INSERT INTO SCHEMA_CHANGE_LOGS VALUES(null,'$version','$app_version','$su_num','$username','${ddl_create}','CREATE TABLE','$create',NOW())"; 2>&1 | grep -v "Warning: Using a password"
+                        mysql -u${username} -h${hostname} -p${password} ${database} -e "INSERT INTO SCHEMA_CHANGE_LOGS VALUES(null,'$version','$chg','$infdba','$username','${ddl_create}','CREATE TABLE','$create',NOW())"; 2>&1 | grep -v "Warning: Using a password"
 
                         echo "Created Tables:" ${CYAN}$create${NC}
                         echo "Migration ${MAG}$create${NC} was executed successfully"
                         exit 0
                     elif [ ! -z "$ddl_drop" ];then
             
-                        mysql -u${username} -h${hostname} -p${password} ${database} -e "INSERT INTO SCHEMA_CHANGE_LOGS VALUES(null,'$version','$app_version','$su_num','$username','${ddl_drop//;}','DROP TABLE','$create',NOW())"; 2>&1 | grep -v "Warning: Using a password"
+                        mysql -u${username} -h${hostname} -p${password} ${database} -e "INSERT INTO SCHEMA_CHANGE_LOGS VALUES(null,'$version','$chg','$infdba','$username','${ddl_drop//;}','DROP TABLE','$create',NOW())"; 2>&1 | grep -v "Warning: Using a password"
 
 
                         echo "Dropped Tables ${CYAN}$drop${NC}"
@@ -115,7 +114,7 @@ if [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$username" ] && [ ! -z "$h
                         exit 0
                     elif [ ! -z "$ddl_insert" ];then
             
-                        mysql -u${username} -h${hostname} -p${password} ${database} -e "INSERT INTO SCHEMA_CHANGE_LOGS VALUES(null,'$version','$app_version','$su_num','$username','${ddl_insert//;}','INSERT','$create',NOW())"; 2>&1 | grep -v "Warning: Using a password"
+                        mysql -u${username} -h${hostname} -p${password} ${database} -e "INSERT INTO SCHEMA_CHANGE_LOGS VALUES(null,'$version','$chg','$infdba','$username','${ddl_insert//;}','INSERT','$create',NOW())"; 2>&1 | grep -v "Warning: Using a password"
 
 
                         echo "Records were inserted"
@@ -123,7 +122,7 @@ if [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$username" ] && [ ! -z "$h
                         exit 0
                     elif [ ! -z "$ddl_update" ];then
             
-                        mysql -u${username} -h${hostname} -p${password} ${database} -e "INSERT INTO SCHEMA_CHANGE_LOGS VALUES(null,'$version','$app_version','$su_num','$username','${ddl_update//;}','UPDATE','$create',NOW())"; 2>&1 | grep -v "Warning: Using a password"
+                        mysql -u${username} -h${hostname} -p${password} ${database} -e "INSERT INTO SCHEMA_CHANGE_LOGS VALUES(null,'$version','$chg','$infdba','$username','${ddl_update//;}','UPDATE','$create',NOW())"; 2>&1 | grep -v "Warning: Using a password"
 
 
                         echo "Records were updated"
@@ -131,7 +130,7 @@ if [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$username" ] && [ ! -z "$h
                         exit 0
                     elif [ ! -z "$ddl_delete" ];then
             
-                        mysql -u${username} -h${hostname} -p${password} ${database} -e "INSERT INTO SCHEMA_CHANGE_LOGS VALUES(null,'$version','$app_version','$su_num','$username','${ddl_delete//;}','DELETE','$create',NOW())"; 2>&1 | grep -v "Warning: Using a password"
+                        mysql -u${username} -h${hostname} -p${password} ${database} -e "INSERT INTO SCHEMA_CHANGE_LOGS VALUES(null,'$version','$chg','$infdba','$username','${ddl_delete//;}','DELETE','$create',NOW())"; 2>&1 | grep -v "Warning: Using a password"
 
 
                         echo "Records were deleted"
@@ -144,10 +143,10 @@ if [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$username" ] && [ ! -z "$h
         fi              
 
     else
-        echo "To execute a migration, run: -C migrate -s <services-name> -v <version-name> -r <app version> -t <SU num> -c <migration> -u <db-user> -h <db-host> -d <db-name> " 
+        echo "To execute a migration, run: -C migrate -s <services-name> -v <version-name> -r <chg> -t <infdba> -c <migration> -u <db-user> -h <db-host> -d <db-name> " 
         exit 1
     fi	
-elif [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$username" ] && [ ! -z "$hostname" ] && [ ! -z "$database" ] && [ ! -z "$version" ]; then
+elif [ ! -z "$cmds" ] && [ ! -z "$cluster" ] && [ ! -z "$username" ] && [ ! -z "$hostname" ] && [ ! -z "$database" ] && [ ! -z "$version" ]; then
     if [ "$cmds" == "info" ]; then
         #(ls -l $versiondir | awk '{print $9}')
         read -p 'Enter DB password:' -s PASSWD
@@ -201,18 +200,17 @@ elif [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$username" ] && [ ! -z "
             (ls $versiondir | tr '\n' '\n' | grep $y)
         done
     else
-        echo "To list pending migrations, run: -C info -s <services-name> -v <version-name> -u <db-user> -h <db-host> -d <db-name>" 
+        echo "To list pending migrations, run: -C info -s <cluster> -v <version> -u <db-user> -h <db-host> -d <db-name>" 
         exit 1
     fi
-elif [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$username" ] && [ ! -z "$hostname" ] && [ ! -z "$database" ]; then
-    init_schemas="defaults/$service/"
+elif [ ! -z "$cmds" ] && [ ! -z "$cluster" ] && [ ! -z "$username" ] && [ ! -z "$hostname" ] && [ ! -z "$database" ]; then
+    init_schemas="defaults/$cluster/"
     if [ "$cmds" == "pull" ]; then
 
         read -p 'Enter DB password:' -s PASSWD
         echo
         password=$PASSWD
 
-        #mysqldump -u${username} -p${password}  -h${hostname} ${database} --no-data --compact --skip-comments --triggers --events --routines --set-gtid-purged=OFF > $init_schemas/$database.sql &&
         mysqldump -u${username} -p${password}  -h${hostname} ${database} --compact --skip-comments --triggers --events --routines --set-gtid-purged=OFF > $init_schemas/init.sql &&
         echo "Data import for $database done"
 	exit 0
@@ -229,7 +227,7 @@ elif [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$username" ] && [ ! -z "
     fi             
 elif [ ! -z "$cmds" ] && [ ! -z "$username" ] && [ ! -z "$hostname" ] && [ ! -z "$database" ]; then
     if [ "$cmds" == "base" ]; then
-        init_schemas="defaults/$service/"
+        init_schemas="defaults/$cluster/"
 
         read -p 'Enter DB password:' -s PASSWD
         echo
@@ -241,7 +239,7 @@ elif [ ! -z "$cmds" ] && [ ! -z "$username" ] && [ ! -z "$hostname" ] && [ ! -z 
         echo "To restore data, run: -C base -s <services-name> -u <db-user> -h <db-host> -d <db-name>" 
         exit 1
     fi      
-elif [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$create" ] && [ ! -z "$version" ]; then
+elif [ ! -z "$cmds" ] && [ ! -z "$cluster" ] && [ ! -z "$create" ] && [ ! -z "$version" ]; then
     if [ "$cmds" == "create" ]; then
         ddl_create=$(echo $create | grep -ioP create)
         ddl_alter=$(echo $create | grep -ioP alter)
@@ -285,15 +283,15 @@ elif [ ! -z "$cmds" ] && [ ! -z "$service" ] && [ ! -z "$create" ] && [ ! -z "$v
         echo "To create a new migration, run: -C create <migration-type> -s <services-name> -v <version-name>"
         exit 1
     fi
-elif [ ! -z "$cmds" ] && [ ! -z "$service" ]; then
+elif [ ! -z "$cmds" ] && [ ! -z "$cluster" ]; then
     if [ "$cmds" == "version" ]; then
-        mkdir $servicedir/"V__$now"
-        echo "Migration version for $service has been created: " ${BLUE}$version${NC}
+        mkdir $clusterdir/"V__$now"
+        echo "Migration version for $cluster has been created: " ${BLUE}$version${NC}
     elif [ "$cmds" == "list" ]; then
         (ls -l $versiondir | awk '{print $9}')
     else
-        echo "To create a new version, run: -C version -s <services-name>"
-        echo "To list existing versions, run: -C list -s <services-name>"
+        echo "To create a new version, run: -C version -s <cluster>"
+        echo "To list existing versions, run: -C list -s <cluster>"
         exit 1
     fi
 else
--- scripts/schema_change_logs.sql
+++ scripts/schema_change_logs.sql
@@ -1,12 +1,13 @@
 CREATE TABLE `SCHEMA_CHANGE_LOGS` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `infdba_number` varchar(32) NOT NULL,
+  `version` varchar(32) NOT NULL,
   `chg_number` varchar(32) NOT NULL,
+  `infdba_number` varchar(32) NOT NULL,
   `executed_by` varchar(32) NOT NULL,
   `table_schemas` varchar(255) NOT NULL DEFAULT '',
   `command` varchar(32) NOT NULL DEFAULT '',
   `script` varchar(255) NOT NULL DEFAULT '',
   `executed_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unq_version_script` (`db_version`,`script`)
+  UNIQUE KEY `unq_version_script` (`version`,`script`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
